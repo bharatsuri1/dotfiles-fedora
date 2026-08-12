30397f5a9b1fc9093d5b19dd1db9a531e7926d4e
Bharat Suri <bharatsuri.us@gmail.com>
2026-08-12 15:37:02 -0700

    Guard package-specific shell aliases

--- config/zsh/aliases.zsh
+++ config/zsh/aliases.zsh
@@ -3,25 +3,34 @@ alias ...='cd ../..'
 alias ....='cd ../../..'
 
 alias c='clear'
-alias l='eza -lah --group-directories-first --icons=auto'
-alias ll='eza -lah --group-directories-first --icons=auto'
-alias la='eza -a --group-directories-first --icons=auto'
-alias tree='eza --tree --group-directories-first --icons=auto'
-alias cat='bat'
-alias catp='bat --plain --paging=never'
-alias grep='rg --color=auto'
 alias mkdir='mkdir -p'
-alias vim='nvim'
 alias which='type -a'
 alias path='print -l $path'
 alias reload='exec zsh'
 
-alias g='git'
-alias gd='git diff'
-alias gds='git diff --staged'
-alias glog='git log --oneline --graph --decorate'
-alias gs='git status --short --branch'
-alias lg='lazygit'
+if command -v eza >/dev/null; then
+  alias l='eza -lah --group-directories-first --icons=auto'
+  alias ll='eza -lah --group-directories-first --icons=auto'
+  alias la='eza -a --group-directories-first --icons=auto'
+  alias tree='eza --tree --group-directories-first --icons=auto'
+fi
+
+if command -v bat >/dev/null; then
+  alias cat='bat'
+  alias catp='bat --plain --paging=never'
+fi
+
+command -v rg >/dev/null && alias grep='rg --color=auto'
+command -v nvim >/dev/null && alias vim='nvim'
+command -v lazygit >/dev/null && alias lg='lazygit'
+
+if command -v git >/dev/null; then
+  alias g='git'
+  alias gd='git diff'
+  alias gds='git diff --staged'
+  alias glog='git log --oneline --graph --decorate'
+  alias gs='git status --short --branch'
+fi
 
 mkcd() {
   mkdir -p -- "$1" && cd -- "$1"
